cb3f70ee04c48a6d0b039678c34ee42d4af18813
vignesh235 <vickyjai003@gmail.com>
2023-06-13 20:00:56 +0530

    change the design of login page

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color primaryColor = const Color(0xff059C4F);
+  static Color primaryColor1 = const Color(0xff06c663);
   static Color secondaryColor = const Color(0xFFeefaf6);
   static Color scaffoldBackgroundColor = const Color(0xFFEDEFFE);
   static Color iconcolor = const Color(0xFFff8000);
